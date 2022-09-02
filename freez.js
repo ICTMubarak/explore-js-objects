a9e67e7d80b8e3f0cc58239f8c7cbad7b13038d2
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-09-02 21:49:54 +0600

    up to pair

--- freez.js
+++ freez.js
@@ -1,3 +1,16 @@
 const bottle = {color: 'yello', price: 50, inClened: true, capacity: 1};
 
+
 console.log(bottle);
+
+const keys = Object.keys(bottle);
+
+console.log(keys);
+
+const values = Object.values(bottle);
+
+console.log(values);
+
+const pair =Object.entries(bottle);
+
+console.log(pair);
